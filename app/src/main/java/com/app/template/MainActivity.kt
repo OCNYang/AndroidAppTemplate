ba4cae60fa47fc31115ac86a3c1f1a33f51eae21
OCNYang <ocnyang@gmail.com>
2024-04-24 17:47:37 +0800

    add SplashScreen；

--- app/src/main/java/com/app/template/MainActivity.kt
+++ app/src/main/java/com/app/template/MainActivity.kt
@@ -6,18 +6,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.app.base.BaseActivity
 import com.app.template.page.TemplateApp
 import com.app.template.ui.theme.AndroidAppTemplateTheme
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setKeepOnScreenCondition {
+            false // 这里可以设置一个变量，等待首页加载完成再显示
+        }
+
         super.onCreate(savedInstanceState)
 
         setContent {
             AndroidAppTemplateTheme(dynamicColor = false) {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     TemplateApp()
+
                 }
             }
         }
